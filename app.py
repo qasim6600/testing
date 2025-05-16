13700c55474981a12abdcb935483c7d445248ce5
qasim6600 <61554356+qasim6600@users.noreply.github.com>
2025-05-16 16:10:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import gradio as gr
 def greet(name):
     return f"Hello {name}!"
 iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch(server_name="0.0.0.0", server_port=7860)
+iface.launch(server_name="0.0.0.0", server_port=80)
